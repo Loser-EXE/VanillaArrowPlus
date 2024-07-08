411498b19137ab83afe6a84bbce37c0b3f8b8b14
Loser-EXE <christopherlemus@protonmail.com>
2024-07-07 21:44:06 -0500

    Made ModdedArrowItem much safer.

--- src/main/java/com/loserexe/vanillaarrowplus/item/ModItems.java
+++ src/main/java/com/loserexe/vanillaarrowplus/item/ModItems.java
@@ -15,15 +15,15 @@ import java.util.List;
 public class ModItems {
     private static final List<Item> arrows = new ArrayList<>();
 
-    public static final Item AMETHYST_ARROW = register("amethyst_arrow", new ModdedArrowItem<>(AmethystArrowEntity.class));
-    public static final Item BLAZING_ARROW = register("blazing_arrow", new ModdedArrowItem<>(BlazingArrowEntity.class));
-    public static final Item CARROT_ARROW = register("carrot_arrow", new ModdedArrowItem<>(CarrotArrowEntity.class));
-    public static final Item ECHO_ARROW = register("echo_arrow", new ModdedArrowItem<>(EchoArrowEntity.class));
-    public static final Item GOLD_ARROW = register("gold_arrow", new ModdedArrowItem<>(GoldArrowEntity.class));
-    public static final Item HONEY_ARROW = register("honey_arrow", new ModdedArrowItem<>(HoneyArrowEntity.class));
-    public static final Item IRON_ARROW = register("iron_arrow", new ModdedArrowItem<>(IronArrowEntity.class));
-    public static final Item QUARTZ_ARROW = register("quartz_arrow", new ModdedArrowItem<>(QuartzArrowEntity.class));
-    public static final Item REDSTONE_ARROW = register("redstone_arrow", new ModdedArrowItem<>(RedstoneArrowEntity.class));
+    public static final Item AMETHYST_ARROW = register("amethyst_arrow", new ModdedArrowItem<>(AmethystArrowEntity::new));
+    public static final Item BLAZING_ARROW = register("blazing_arrow", new ModdedArrowItem<>(BlazingArrowEntity::new));
+    public static final Item CARROT_ARROW = register("carrot_arrow", new ModdedArrowItem<>(CarrotArrowEntity::new));
+    public static final Item ECHO_ARROW = register("echo_arrow", new ModdedArrowItem<>(EchoArrowEntity::new));
+    public static final Item GOLD_ARROW = register("gold_arrow", new ModdedArrowItem<>(GoldArrowEntity::new));
+    public static final Item HONEY_ARROW = register("honey_arrow", new ModdedArrowItem<>(HoneyArrowEntity::new));
+    public static final Item IRON_ARROW = register("iron_arrow", new ModdedArrowItem<>(IronArrowEntity::new));
+    public static final Item QUARTZ_ARROW = register("quartz_arrow", new ModdedArrowItem<>(QuartzArrowEntity::new));
+    public static final Item REDSTONE_ARROW = register("redstone_arrow", new ModdedArrowItem<>(RedstoneArrowEntity::new));
 
     private static Item register(String name, Item item) {
         Item registeredItem = Registry.register(Registries.ITEM, Identifier.of(VanillaArrowPlus.MOD_ID, name), item);
