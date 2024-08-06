b4e0bd53a27da0967c4a1eb86aec31d9c8b3c3fd
LoserEXE <christopherlemus@protonmail.com>
2024-08-06 12:36:49 -0500

    Added Echo Arrow homing.

--- src/main/java/com/loserexe/vanillaarrowplus/entity/projectile/EchoArrowEntity.java
+++ src/main/java/com/loserexe/vanillaarrowplus/entity/projectile/EchoArrowEntity.java
@@ -10,18 +10,24 @@ import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class EchoArrowEntity extends PersistentProjectileEntity {
+    private LivingEntity target;
+
     public EchoArrowEntity(World world, ItemStack stack, LivingEntity owner, @Nullable ItemStack shotFrom) {
         super(ModEntityTypes.ECHO_ARROW, owner, world, stack, shotFrom);
 
         ((PersistentProjectileEntityAccessor) (this)).setDamage(6);
+
+        this.target = getTarget();
     }
 
     public EchoArrowEntity(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
@@ -33,16 +39,25 @@ public class EchoArrowEntity extends PersistentProjectileEntity {
         super.onEntityHit(entityHitResult);
     }
 
+    private LivingEntity getTarget() {
+        List<Entity> entityList = this.getWorld().getOtherEntities(this, Box.of(this.getOwner().getPos(), 32, 32, 32));
+        entityList = entityList.stream().filter(entity -> entity instanceof LivingEntity && entity != this.getOwner()).toList();
+
+        try {
+            return (LivingEntity) entityList.getFirst();
+        } catch(NoSuchElementException e) {
+            return null;
+        }
+    }
+
     @Override
     public void tick() {
         super.tick();
 
-        if(this.getOwner() == null) {
-            return;
+        if(target != null && target.isAlive()) {
+            Vec3d a = target.getPos().subtract(this.getPos());
+            this.setVelocity(a.normalize());
         }
-
-        List<Entity> entityList = this.getWorld().getOtherEntities(this, Box.of(this.getOwner().getPos(), 32, 32, 32));
-        entityList = entityList.stream().filter(entity -> (entity instanceof LivingEntity)).toList();
     }
 
     @Override
